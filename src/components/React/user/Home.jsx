bc03a64bb55ee54a945ae36ba935ae28a515886f
Krish <7932krishraninga@gmail.com>
2025-10-24 13:32:57 +0530

    Describe your changes here

--- src/components/React/user/Home.jsx
+++ src/components/React/user/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './header'
+import Header from './Header'
 import Banner from './Banner'
 import Package from './Package'
 import Supplement from './Supplement'
